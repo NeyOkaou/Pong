fa0ffd54abb82e041272cc635d38508db796bf3a
NeyOkaou <143295893+NeyOkaou@users.noreply.github.com>
2025-04-25 14:10:42 +0200

    Ball
    Added a ball but i cannot test right now if it bounces properly

---
+++ entities/Ball/Ball.gd
@@ -0,0 +1,35 @@
+extends CharacterBody2D
+
+var initial_ball_speed = 300
+var speed_commulator  = 50
+var ball_speed = initial_ball_speed
+
+var hit_counter = 0
+var max_hit_counter = 12
+var direction = Vector2()
+
+func _ready():
+	#randomize the seed for the generated numbers
+	randomize()
+	set_start_direction()
+
+func set_start_direction():
+	#decide the direction the ball will go in first
+	var random_x = 0
+	if randi()% 10 < 5:
+		random_x = 1
+		random_x = -1
+	direction = Vector2(random_x,randf_range(-1, 1))
+	direction = direction.normalized() * ball_speed
+	
+func _physics_process(delta):
+	var collision = move_and_collide(direction * delta)
+	#make the ball bounce when it touch the wall
+	if collision:
+		direction = direction.bounce(collision.normal)
+	#make the ball bounce when it touch a racket
+	if collision.collider.is_in_groupe("rackets"):
+		direction = direction.normalized() * ball_speed + hit_counter * speed_commulator
+		#add one to the counter if under the max_count limit
+		if hit_counter < max_hit_counter :
+			hit_counter += 1
